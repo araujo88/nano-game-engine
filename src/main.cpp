9e8b682328a006ce71df4bd078324754431553e7
Leonardo Araujo <leonardo.aa88@gmail.com>
2024-04-02 02:30:01 -0300

    refactor: main function

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,19 @@
 #include "../include/world.hpp"
 
 int main(void) {
-  nano::Game game = nano::Game("Nano Game Engine");
+  auto game = nano::Game("Nano Game Engine");
 
-  nano::World world = nano::World(game.getRenderer(), "sprites/grass.png");
+  auto renderer = game.getRenderer();
 
-  nano::Player player =
-      nano::Player("Player", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 32,
-                   game.getRenderer(), "sprites/wojak.png");
+  auto world = nano::World(renderer, "sprites/grass.png");
+
+  auto player = nano::Player("Player", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 32,
+                             renderer, "sprites/wojak.png");
 
   std::vector<nano::NPC *> npcs;
 
   for (int i = 0; i < 10; ++i)
-    npcs.push_back(
-        new nano::NPC("NPC", 0, 0, game.getRenderer(), "sprites/chud.png"));
+    npcs.push_back(new nano::NPC("NPC", 0, 0, renderer, "sprites/chud.png"));
 
   game.addEntity(&world);
   game.addEntity(&player);
